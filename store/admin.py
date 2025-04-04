10cad51d088779b9cfbb23e0a611954287eeb7c4
PenDatabase <davideneasatochibueze@gmail.com>
2025-04-04 13:59:17 +0100

    Created Ordered quantity field in admin section for each product so that admin can know quantity of each product to buy

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from django.db.models import Sum
+from django.db.models import Sum, Count, F, Case, When, Value, IntegerField
 from . import models
 
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    exclude = ['code'] # remove code when creating order so as to automatically generate
     list_display = ['code', 'user__email', 'date']
     list_per_page = 10
     search_fields = ['user__email', 'code']
+    readonly_fields = ['code'] # Make code Read Only field so as to prevent from being edited
 
 
 @admin.register(models.OrderItem)
@@ -31,11 +31,22 @@ class ProductAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='ordered_quantity')
     def ordered_quantity(self, product: models.Product):
-        return product.ordered_quantity or 0
+        return product.ordered_quantity
     
     def get_queryset(self, request):
         return models.Product.objects.annotate(
-            ordered_quantity = Sum('package_items__package__order_items__quantity')
+            ordered_quantity = Sum(
+                Case(
+                        When(package_items__quantity=0, then=Value(1)),
+                        default=F('package_items__quantity'),
+                        output_field=IntegerField()
+                    ) *
+                Case(
+                    When(package_items__package__order_items__quantity=0, then=Value(1)),
+                    default=F('package_items__package__order_items__quantity'),
+                    output_field=IntegerField()
+                ),
+            )
         )
 
 
@@ -45,8 +56,18 @@ class PackageItemAdmin(admin.ModelAdmin):
 
 
 @admin.register(models.Cart)
-class Cart(admin.ModelAdmin):
-    list_display = ['user', 'created_at']
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'number_of_items']
+
+    def number_of_items(self, cart):
+        return cart.number_of_items
+
+    def get_queryset(self, request):
+        return models.Cart.objects.annotate(
+            number_of_items=Count("cart_items")
+        )
 
 
-admin.site.register(models.CartItem)
+@admin.register(models.CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'package', 'quantity', 'total_price']
